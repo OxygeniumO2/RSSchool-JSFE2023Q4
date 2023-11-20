367da2eda37d339ffeff930aed5ccd7756ec04e2
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2023-11-20 21:08:53 +0200

    fix: links in header, double code from js

--- coffee-house/js/menu-page.js
+++ coffee-house/js/menu-page.js
@@ -1,18 +1,6 @@
 'use strict';
 
+import { smoothScrolling } from "./default.js";
+
 const headerMenuListUl = document.querySelector('.header__nav__list');
-
-function smoothScrolling(item) {
-  const link = item.target;
-  if (link.classList.contains('header__menu__link')) {
-    const gotoBlock = document.querySelector(link.dataset.goto);
-    const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
-    window.scrollTo({
-      top: gotoBlockValue,
-      behavior: "smooth",
-    });
-    e.preventDefault();
-  }
-};
-
 headerMenuListUl.addEventListener('click', smoothScrolling);
