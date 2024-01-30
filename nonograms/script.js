f9de0903b05fda87f15970bcbba90fa4b99cf08a
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-01-30 22:15:44 +0200

    feat: left hints generation, change build func

--- nonograms/script.js
+++ nonograms/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { nonogramsData } from './data.js';
+
 function createElem({tag, classesCss}) {
   const elem = document.createElement(tag);
 
@@ -13,45 +15,73 @@ function createElem({tag, classesCss}) {
 
 const gamefield = createElem({ tag: 'div', classesCss: ['gamefield'] });
 
-function buildGame(value) {
+const main = createElem({ tag: 'main' });
+document.body.append(main);
 
-  document.body.innerHTML = ``;
+const container = createElem({ tag: 'div', classesCss: ['container'] });
+main.append(container);
 
-  const main = createElem({ tag: 'main' });
-  document.body.append(main);
+const nonogramsContainer = createElem({ tag: 'div', classesCss: ['nonograms__container'] });
+const nonogramsLeftColumn = createElem({ tag: 'div', classesCss: ['nonograms__leftcolumn'] });
+const nonogramsRightColumn = createElem({ tag: 'div', classesCss: ['nonograms__rightcolumn'] });
 
-  const container = createElem({ tag: 'div', classesCss: ['container'] });
-  main.append(container);
+container.append(nonogramsContainer);
 
-  const nonogramsContainer = createElem({ tag: 'div', classesCss: ['nonograms__container'] });
-  const nonogramsLeftColumn = createElem({ tag: 'div', classesCss: ['nonograms__leftcolumn'] });
-  const nonogramsRightColumn = createElem({ tag: 'div', classesCss: ['nonograms__rightcolumn'] });
+nonogramsContainer.append(nonogramsLeftColumn, nonogramsRightColumn);
 
-  container.append(nonogramsContainer);
+const emptyBlock = createElem({ tag: 'div', classesCss: ['empty__block'] });
+const hintsLeft = createElem({ tag: 'div', classesCss: ['hints__left'] });
 
-  nonogramsContainer.append(nonogramsLeftColumn, nonogramsRightColumn);
+nonogramsLeftColumn.append(emptyBlock, hintsLeft);
 
-  const emptyBlock = createElem({ tag: 'div', classesCss: ['empty__block'] });
-  const hintsLeft = createElem({ tag: 'div', classesCss: ['hints__left'] });
-
-  nonogramsLeftColumn.append(emptyBlock, hintsLeft);
-
-  const hintsTop = createElem({ tag: 'div', classesCss: ['hints__top'] });
+const hintsTop = createElem({ tag: 'div', classesCss: ['hints__top'] });
 
 
-  nonogramsRightColumn.append(hintsTop, gamefield);
+nonogramsRightColumn.append(hintsTop, gamefield);
+
+function buildGame(value, title) {
+
+  const currNonogram = nonogramsData.find((item) => item.title === title);
+
 
   for (let i = 0; i < value * value; i += 1) {
     const cell = createElem({ tag: 'div', classesCss: ['gamefield__cell'] });
     gamefield.append(cell);
-
     if (i < value) {
       const hintLeft = createElem({ tag: 'div', classesCss: ['hints__left__item'] });
       const hintTop = createElem({ tag: 'div', classesCss: ['hints__top__item'] });
+      hintLeft.innerHTML = calculateLeftHints(currNonogram, i);
+      hintTop.innerHTML = calculateTopHints(currNonogram, i);
       hintsLeft.append(hintLeft);
       hintsTop.append(hintTop);
     }
   }
+
 }
 
-buildGame(5);
+function calculateLeftHints(nonogram, i) {
+
+  let temp = 0;
+
+  const res = nonogram.nonogramArr[i].reduce((acc, item, index) => {
+    if (item === 1) {
+      temp += 1;
+    }
+    if (item === 0) {
+      acc.push(temp);
+      temp = 0;
+    }
+    if (index === nonogram.nonogramArr[i].length - 1) {
+      acc.push(temp);
+      temp = 0;
+    }
+    return acc;
+  }, []);
+  const leftHint = res.join(' ').replaceAll('0', '');
+  return leftHint;
+}
+
+function calculateTopHints(nonogram, i) {
+}
+
+buildGame(5, 'ladder');
