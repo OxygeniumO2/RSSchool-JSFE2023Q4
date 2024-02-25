866553487fef081c28c89a8cf7317f94c1fb3185
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-02-25 14:36:00 +0200

    feat: appLoader.js to appLoader.ts

---
+++ news-api/src/components/controller/appLoader.ts
@@ -0,0 +1,13 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+    constructor() {
+        if (process.env.API_URL && process.env.API_KEY) {
+            super(process.env.API_URL, {
+                apiKey: process.env.API_KEY,
+            });
+        }
+    }
+}
+
+export default AppLoader;
