44bd18cfa14d197b8ed5e6f780f2c62a4565c6e8
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-03-05 20:11:09 +0200

    init: start task rss-puzzle

---
+++ rss-puzzle/webpack.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const EslingPlugin = require('eslint-webpack-plugin');
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new EslingPlugin({ extensions: 'ts' })
+  ],
+  devServer: {
+    static: path.join(__dirname, 'src'),
+    compress: true,
+    port: 8080,
+    hot: true,
+  },
+};
