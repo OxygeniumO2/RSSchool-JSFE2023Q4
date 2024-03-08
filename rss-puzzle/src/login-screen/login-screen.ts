a0434d92ea57aaefd1659265b7e127b294e3c025
OxygeniumO2 <96070870+OxygeniumO2@users.noreply.github.com>
2024-03-08 22:32:20 +0200

    feat: form validation, some changes in create elem function (#45)

--- rss-puzzle/src/login-screen/login-screen.ts
+++ rss-puzzle/src/login-screen/login-screen.ts
@@ -2,6 +2,16 @@ import './login-screen.css';
 import { createElem } from '../utils/createElem';
 import { container } from '../app-container/container';
 
+// const VALIDATE_REGEX: RegExp = /^[A-Z][-a-zA-Z]+$/;
+const MINIMUM_FIRSTNAME_LENGTH: number = 3;
+const MINIMUM_LASTNAME_LENGTH: number = 4;
+const INPUT_REGEX: string = '[A-Z][a-z]*-?[a-z]*';
+const INPUT_PLACEHOLDER_DEFAULT: string = 'First letter must be uppercase';
+const INPUT_TITLE_FIRSTNAME_DEFAULT: string = 'First letter uppercase, English alphabet, at least 3 characters';
+const INPUT_TITLE_LASTNAME_DEFAULT: string = 'First letter uppercase, English alphabet, at least 4 characters';
+
+type ValidateUserInfo = (name: string, lastName: string) => void;
+
 function createLoginScreen() {
   const loginScreen = createElem({ tag: 'div', classesCss: ['login__screen'] });
   const loginScreenFormContainer = createElem({ tag: 'form', classesCss: ['login__screen__form-container'] });
@@ -9,15 +19,29 @@ function createLoginScreen() {
 
   const labelFirstName = createElem({ tag: 'label' });
   const firstNameSpan = createElem({ tag: 'span', textContent: 'First name' });
-  const firstNameInput = createElem({ tag: 'input', type: 'text' }) as HTMLInputElement;
-  firstNameInput.required = true;
+  const firstNameInput = createElem({
+    tag: 'input',
+    type: 'text',
+    required: true,
+    minLength: MINIMUM_FIRSTNAME_LENGTH,
+    pattern: INPUT_REGEX,
+    placeholder: INPUT_PLACEHOLDER_DEFAULT,
+    title: INPUT_TITLE_FIRSTNAME_DEFAULT,
+  }) as HTMLInputElement;
 
   labelFirstName.append(firstNameSpan, firstNameInput);
 
   const labelLastName = createElem({ tag: 'label' });
   const lastNameSpan = createElem({ tag: 'span', textContent: 'Surname' });
-  const lastNameInput = createElem({ tag: 'input', type: 'text' }) as HTMLInputElement;
-  lastNameInput.required = true;
+  const lastNameInput = createElem({
+    tag: 'input',
+    type: 'text',
+    required: true,
+    minLength: MINIMUM_LASTNAME_LENGTH,
+    pattern: INPUT_REGEX,
+    placeholder: INPUT_PLACEHOLDER_DEFAULT,
+    title: INPUT_TITLE_LASTNAME_DEFAULT,
+  }) as HTMLInputElement;
 
   labelLastName.append(lastNameSpan, lastNameInput);
 
@@ -30,6 +54,15 @@ function createLoginScreen() {
 
   loginScreenFormContainer.append(labelFirstName, labelLastName, loginScreenBtn);
   container.append(loginScreen);
+
+  loginScreenFormContainer.addEventListener('submit', function (event) {
+    event.preventDefault();
+    validateUserInfo(firstNameInput.value, lastNameInput.value);
+  });
 }
+
+const validateUserInfo: ValidateUserInfo = (name, lastName) => {
+  console.log(name, lastName);
+};
 
 export default createLoginScreen;
