402ddad70efa297e2fe482c52dae38bedae7a9f4
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-04-10 19:25:39 +0300

    rename: file name without and

--- rss-puzzle/src/app-container/init-app.ts
+++ rss-puzzle/src/app-container/init-app.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import checkLSAndGenLogOrStartScreen from '../utils/checkLSAndGenLogOrStartScreen';
+import checkLSAndGenLogOrStartScreen from '../utils/logOrStartScreen';
 import { appendRoot } from './container';
 
 function init() {
