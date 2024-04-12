3a065dafb99192d8ecdb3531f336953a48f1d15e
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-04-12 20:15:28 +0300

    init: fun-chat project

---
+++ fun-chat/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "./src/index.ts"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  resolve: {
+    extensions: ['', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src/index.html"),
+        filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new EslintPlugin({ extensions: 'ts' }),
+],
+module: {
+  rules: [
+    {
+      test: /\.(jpg|png|svg|jpeg|gif)$/,
+      type: 'asset/resource',
+    },
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
+    },
+    {
+      test: /\.ts$/i,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    },
+  ],
+ },
+};
