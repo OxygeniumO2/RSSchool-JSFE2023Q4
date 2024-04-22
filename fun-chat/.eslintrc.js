04368efaeab9e4bb7da6a12dfe38db3b65b8b036
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-04-22 12:52:03 +0300

    edit: eslint noInlineConfig, but i cant handle with dependancy cycle

--- fun-chat/.eslintrc.js
+++ fun-chat/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         "es2021": true,
         "node": false
     },
-    // "noInlineConfig": true,
+    "noInlineConfig": true,
     "extends": [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
