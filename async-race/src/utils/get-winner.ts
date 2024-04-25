41268c697ad0843dfc234dc5d926ecfd4fc32fe5
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-04-25 22:59:36 +0300

    fix: unnecessary variable declaration

--- async-race/src/utils/get-winner.ts
+++ async-race/src/utils/get-winner.ts
@@ -1,6 +1,5 @@
 async function getWinnerResp(url: string, path: string, carId: number) {
-  const currCarByIdResponse = await fetch(`${url}${path}/${carId}`);
-  return currCarByIdResponse;
+  return fetch(`${url}${path}/${carId}`);
 }
 
 export default getWinnerResp;
